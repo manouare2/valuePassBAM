740984d2d060ca89dd12da868069763f858d9add
youssef manouare <youssef.manouare@gmail.com>
2021-12-07 18:07:44 +0100

    integrate testFairy

--- app/src/main/java/com/example/karim/applicationfacteur/AgencyApplication.java
+++ app/src/main/java/com/example/karim/applicationfacteur/AgencyApplication.java
@@ -1,9 +1,12 @@
 package com.example.karim.applicationfacteur;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+import com.testfairy.TestFairy;
 
 
-public class AgencyApplication extends Application {
+public class AgencyApplication extends MultiDexApplication {
 
 	@Override
 	public void onCreate() {
@@ -12,7 +15,7 @@ public class AgencyApplication extends Application {
 		//Initialize logging for debugging
         TraceLog.initialize(this);
         TraceLog.scoped(this).d("onCreate");
-
+		TestFairy.begin(this, "SDK-Gt2ba5r1");
 	}
 
 }
